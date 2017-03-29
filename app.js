028d6d09007b8ec50eae0d87307fed4c6a5ddd59
Alex Bezuska <abezuska@gmail.com>
2017-03-29 12:41:04 -0400

    refine some things, improve naming

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var fs = require('fs');
 
-var prefixes = makeList("./lists/prefixes.txt");
-var descriptives = makeList("./lists/descriptives.txt");
-var places = makeList("./lists/places.txt");
-var suffixes = makeList("./lists/suffixes.txt");
+/* Location lists */
+locationPrefixes = makeList("./lists/location-prefixes.txt");
+adjectives = makeList("./lists/adjectives.txt");
+locations = makeList("./lists/locations.txt");
+locationSuffixes = makeList("./lists/location-suffixes.txt");
 
-var titles = makeList("./lists/titles.txt");
-var firstnames = makeList("./lists/firstnames.txt");
-var lastnames = makeList("./lists/lastnames.txt");
+/* Username lists */
+titles = makeList("./lists/titles.txt");
+firstnames = makeList("./lists/firstnames.txt");
+lastnames = makeList("./lists/lastnames.txt");
 
+function makeList(file) {
+  return fs.readFileSync(file).toString().trim().split("\n");
+}
 
 var random = {
   "inRange": function(min, max) {
@@ -33,32 +38,32 @@ function generateName(parts) {
   return names.join(" ");
 }
 
-function makeList(file) {
-  return fs.readFileSync(file).toString().trim().split("\n");
-}
+/* For testing purposes only
+Logs a sample group of names to the console
+*/
 var results = 30;
-
-console.log("Room possibilities: ", prefixes.length * descriptives.length * places.length * suffixes.length);
-for (var i = 0; i < results; i++){
-  var name = generateName(
+console.log("Locations:");
+for (var i = 0; i < results; i++) {
+  var locationName = generateName(
     [
-      { list: prefixes, probability: 0.5 },
-      { list: descriptives, probability: 1 },
-      { list: places, probability: 1 },
-      { list: suffixes, probability: 0.2}
+      { list: locationPrefixes, probability: 0.5 },
+      { list: adjectives, probability: 1 },
+      { list: locations, probability: 1 },
+      { list: locationSuffixes, probability: 0.2 }
     ]
   );
-  console.log(name);
-
+  console.log(locationName);
 }
-console.log("User possibilities: ", titles.length * firstnames.length * lastnames.length);
-for (var i = 0; i < results; i++){
-  var name = generateName(
+
+console.log("Users:");
+for (var i = 0; i < results; i++) {
+  var userName = generateName(
     [
       { list: titles, probability: 1 },
+      { list: adjectives, probability: 0.1 },
       { list: firstnames, probability: 1 },
       { list: lastnames, probability: 1 }
     ]
   );
-  console.log(name);
+  console.log(userName);
 }
