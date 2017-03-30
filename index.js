aed9e8261a4a88fe57ed7d359e43e4e5e00bc7d5
abezuska <abezuska@gmail.com>
2017-03-30 16:35:16 -0400

    use express to serve static website

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ var generator = require('./app');
 
 var app = express();
 
+app.use(express.static('public'));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.use('/', express.static('public'));
 
 app.get('/location', function(req, res) {
   res.json( generator.location());
